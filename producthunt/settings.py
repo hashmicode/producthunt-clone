29a9165309724d520e4467544a6f2565df3aaedf
Khurram <hashmi.khurram786@gmail.com>
2019-08-27 01:42:23 +0530

    new changes

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'producthuntdb',
         'USER': 'postgres',
-        'PASSWORD': '123@Savant',
+        'PASSWORD': '',
         'HOST':'localhost',
         'PORT': '5432',
     }
